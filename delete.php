5855c4e89ecdda5116ffa32ced1b53de60d2bda2
DimitriGIGAN <67626317+DimitriGIGAN@users.noreply.github.com>
2021-04-05 21:45:00 +0400

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,23 @@
+<?php
+$id=$_GET['id'];
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("185.98.131.148", "servi1617577", "Simplon974", "servi1617577");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt delete query execution
+$sql = "DELETE FROM employees WHERE id=".$id."";
+if(mysqli_query($link, $sql)){
+    echo "Records were deleted successfully.";
+    header("location:question.php");
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+?>
